dfff9f72183ebe078384ea8e8d31d48e3e87f7a8
Lem0nY <connor.mchugh5@gmail.com>
2019-09-12 20:16:25 +1000

    testing

--- WiFi-BomB.sh
+++ WiFi-BomB.sh
@@ -47,7 +47,7 @@ function kill_scan() {
 function start_wifite() {
 	$(kill_scan) &
 	# expect -f inputs.exp
-	print "all" | wifite -quiet -wpa -wpat 120 -i wlan1
+	printf "all" | wifite -quiet -wpa -wpat 120 -i wlan1
 
 }
 
