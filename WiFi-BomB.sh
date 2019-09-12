2f4cb0f9606738bb442405b7fbf97512afa903ec
Lem0nY <connor.mchugh5@gmail.com>
2019-09-13 07:24:57 +1000

    testing

--- WiFi-BomB.sh
+++ WiFi-BomB.sh
@@ -78,7 +78,7 @@ function install_dependencies() {
 		echo y | apt install wifite perl
 
 		# Add to .bashrc
-		echo "sudo sh $FILE_LOCATION" >> ~/.bashrc
+		echo "sudo sh /home/pi/WiFi-BomB/WiFi-BomB" >> /home/pi/.bashrc
 
 		## NOT CURRENTLY WORKING
 		# Install nexmon custom firmware (to allow pi's card to be put in monitor mode)
